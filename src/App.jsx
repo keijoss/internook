363b9aeefffc45ce4eff1af0065650025960f227
mythterncf <127208978+keijoss@users.noreply.github.com>
2025-02-17 10:17:14 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className="App" style={{ backgroundColor: 'blue', height: '100vh', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      App
+    </div>
+  );
+};
+
+export default App;
